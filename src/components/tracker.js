914bf59a0545c8feb3b4eb33c9d370b7836d4a20
Your Name <you@example.com>
2021-02-05 21:56:40 -0600

    added currency logos. wrote media queries

--- src/components/tracker.js
+++ src/components/tracker.js
@@ -10,10 +10,10 @@ export default class BitcoinOpenClose extends Component {
         bitcoinPrice: 0,
         dogeName: "",
         bitcoinName: "",
-        dogeholdings: "",
-        bitholdings: "",
-        currentTime: "",
-        dogeholdingsAmount: 519.5,
+        dogeholdings: "0",
+        bitholdings: "0",
+        currentTime: "Calculating......",
+        dogeholdingsAmount: 519.97,
         bitholdingsAmount: .00013881
       };
   }
@@ -31,11 +31,9 @@ export default class BitcoinOpenClose extends Component {
             let updatedDogecoinPrice = response.data[i].price;
             let newCoinName = response.data[i].currency;
             let dogeHoldings = updatedDogecoinPrice * this.state.dogeholdingsAmount;
-            let liveTime = Date().toLocaleString();
             this.setState({ dogeName: newCoinName });
             this.setState({ dogePrice: updatedDogecoinPrice });
             this.setState({dogeholdings: dogeHoldings});
-            this.setState({currentTime: liveTime})
           }
 
           if (response.data[i].currency === "BTC") {
@@ -48,6 +46,9 @@ export default class BitcoinOpenClose extends Component {
           }
           
         }
+        
+        let liveTime = Date().toLocaleString();
+        this.setState({currentTime: liveTime})
         
       })
       , 5000);
@@ -62,7 +63,8 @@ export default class BitcoinOpenClose extends Component {
             <h1 className="timer">{this.state.currentTime}</h1>
                 <div className="flex">
                     <div className="doge card">
-                        <h1>Dogecoin</h1>
+                        {/* <h1>Dogecoin</h1> */}
+                        <img className="dogeimage" src="https://cnet3.cbsistatic.com/img/JdgcCFyRIZTnn8VtrEGYt0Ccn2k=/940x0/2013/12/27/292438ab-8533-11e3-bc97-14feb5ca9861/dogecoin.jpg" alt="dogecoin"/>
                         <h3>The Current Price Of {this.state.dogeName} Is:</h3>
                         <h3>${this.state.dogePrice}</h3>
                         <br></br>
@@ -70,7 +72,8 @@ export default class BitcoinOpenClose extends Component {
                         <h3>${this.state.dogeholdings}</h3>
                     </div>
                     <div className="bit card">
-                        <h1>Bitcoin</h1>
+                        {/* <h1>Bitcoin</h1> */}
+                        <img className="bitimage" src="https://pbs.twimg.com/profile_images/421692600446619648/dWAbC2wg_400x400.jpeg" alt="bitcoin"/>
                         <h3>The Current Price Of {this.state.bitcoinName} Is:</h3>
                         <h3>${this.state.bitcoinPrice}</h3>
                         <br></br>
